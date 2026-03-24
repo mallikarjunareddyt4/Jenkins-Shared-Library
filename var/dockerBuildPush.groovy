aa603b8678c2caec12ba10ede9e7b6b781f9537c
mallikarjunareddyt4 <mallikarjunareddyt4@gmail.com>
2026-03-24 11:37:14 +0530

    Add Docker build and push pipeline stage

---
+++ var/dockerBuildPush.groovy
@@ -0,0 +1,13 @@
+def call(String imageName) {
+    stage('Docker Build & Push') {
+        withCredentials([usernamePassword(credentialsId: 'DockerHub',
+                                           usernameVariable: 'USER',
+                                           passwordVariable: 'PASS')]) {
+            sh '''
+               echo "$PASS" | docker login -u "$USER" --password-stdin
+               docker build -t ${imageName} .
+               docker push ${imageName}
+            '''
+        }
+    }
+}
